d2b0507e7ee740f0a56dbcde8a1f2b1933d4285e
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-02-09 12:05:11 +0530

    To Do - Data Service methods added

--- 1_ToDoApp/MAUIClient/DataServices/RestDataService.cs
+++ 1_ToDoApp/MAUIClient/DataServices/RestDataService.cs
@@ -33,14 +33,55 @@ namespace MAUIClient.DataServices
             return Connectivity.Current.NetworkAccess == NetworkAccess.Internet;
         }
 
-        public Task AddAsync(ToDo toDo)
+        public async Task AddAsync(ToDo toDo)
         {
-            throw new NotImplementedException();
+            if (!IsInternetAvailable())
+            {
+                Debug.WriteLine("----> No Internet Access");
+                return;
+            }
+            try
+            {
+                string jsonToDo = JsonSerializer.Serialize<ToDo>(toDo, _jsonSerializerOptions);
+                StringContent content = new StringContent(jsonToDo, Encoding.UTF8, "application/json");
+                var response = await _httpClient.PostAsync($"{_baseAddress}/todo", content);
+                if (!response.IsSuccessStatusCode)
+                {
+                    Debug.WriteLine("----> Not Http 2xx responses");
+                    return;
+                }
+                Debug.WriteLine("----> Successfully added to do");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"----> Exception: {ex?.Message}");
+                return;
+            }
         }
 
-        public Task DeleteAsync(int id)
+        public async Task DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            if (!IsInternetAvailable())
+            {
+                Debug.WriteLine("----> No Internet Access");
+                return;
+            }
+            try
+            {
+               
+                var response = await _httpClient.DeleteAsync($"{_baseAddress}/todo/{id}");
+                if (!response.IsSuccessStatusCode)
+                {
+                    Debug.WriteLine("----> Not Http 2xx responses");
+                    return;
+                }
+                Debug.WriteLine($"----> Successfully deleted to do with ID {id}");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"----> Exception: {ex?.Message}");
+                return;
+            }
         }
 
         public async Task<IEnumerable<ToDo>> GetAllToDosAsync()
@@ -77,9 +118,30 @@ namespace MAUIClient.DataServices
             }
         }
 
-        public Task UpdateAsync(ToDo toDo)
+        public async Task UpdateAsync(ToDo toDo)
         {
-            throw new NotImplementedException();
+            if (!IsInternetAvailable())
+            {
+                Debug.WriteLine("----> No Internet Access");
+                return;
+            }
+            try
+            {
+                string jsonToDo = JsonSerializer.Serialize<ToDo>(toDo, _jsonSerializerOptions);
+                StringContent content = new StringContent(jsonToDo, Encoding.UTF8, "application/json");
+                var response = await _httpClient.PutAsync($"{_baseAddress}/todo/{toDo.Id}", content);
+                if (!response.IsSuccessStatusCode)
+                {
+                    Debug.WriteLine("----> Not Http 2xx responses");
+                    return;
+                }
+                Debug.WriteLine("----> Successfully updated to do");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"----> Exception: {ex?.Message}");
+                return;
+            }
         }
     }
 }
